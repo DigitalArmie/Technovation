316cbf7c0ac86735ec13232da6dd148a260b5cde
Miriam Tranca <trancamiriam@gmail.com>
2023-02-09 23:58:56 +0200

    var 2

--- app1/App.js
+++ app1/App.js
@@ -1,40 +1,12 @@
-import React, {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { LinearGradient }from 'expo-linear-gradient';
+import {NavigationContainer} from '@react-navigation/native'
+import Tabs from './navigation/Tabs';
 
-
-export default function App() {
+const App = ( ) => {
   return (
-    <LinearGradient
-      colors={['#ADA0FC','#F6F5FC','#ADA0FC','#1B0B77']}
-      style={{flex: 1}}
-      start={{x:0.2, y:0}}
-      end={{x:1.2, y:1.1}}
-    >
-    <Text style={styles.text1}>Notifications:</Text>
-    <View style={styles.toolbar}></View>
-    </LinearGradient> 
-
+    <NavigationContainer>
+    <Tabs />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  text1: {
-    fontSize: 24,
-    position:'absolute',
-    left:'20%',
-    top: 60,
-    color:'#1B0B77',
-    fontWeight:'bold',
-    letter:'6,5%'
-  },
-  toolbar: {
-    justifyContent:'center',
-    alignItems: 'center',
-    color: '#FFFFFF',
-    
-    left: 0,
-    right: 0
-  }
-});
+export default App;
---
+++ app1/screens/ProfileScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient }from 'expo-linear-gradient';
+
+
+
+export default function profilescreen() {
+  return (
+    <LinearGradient
+      colors={['#ADA0FC','#F6F5FC','#ADA0FC','#1B0B77']}
+      style={{flex: 1}}
+      start={{x:0.2, y:0}}
+      end={{x:1.2, y:1.1}}
+    >
+
+    </LinearGradient> 
+
+  );
+}
+
+const styles = StyleSheet.create({
+  text1: {
+    fontSize: 24,
+    position:'absolute',
+    left:'20%',
+    top: 60,
+    color:'#1B0B77',
+    fontWeight:'bold',
+    letter:'6,5%'
+  }
+});
